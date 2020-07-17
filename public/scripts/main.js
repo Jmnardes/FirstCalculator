7f5906b2a695e0ab956ebb7b246b62594b273923
jmnardes <jmnardes@hotmail.com>
2020-07-17 09:58:24 -0400

    comments added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,40 +1,47 @@
-function Calculadora() {
+function Calculator() {
     this.display = document.querySelector('.display');
-    
+
+    //method for events
     this.start = () => {
         this.btnClick();
         this.pressEnter();
         this.pressBack();
     };
     
+    //event for button click
     this.btnClick = () => {
         document.addEventListener('click', (e) => {
             const el = e.target;
 
-            if (el.classList.contains('btn-num')) this.displayBtn(el);
+            //check the clicked element
+            if(el.classList.contains('btn-num')) this.displayBtn(el);
             if(el.classList.contains('btn-clear')) this.clear();
             if(el.classList.contains('btn-del')) this.del();
             if(el.classList.contains('btn-eq')) this.eq();
         });
     };
 
+    //use keycode to identify enter pressed
     this.pressEnter = () => {
         this.display.addEventListener('keyup', (e) => {
             if(e.keyCode === 13) this.eq();
         });
     };
     
+    //same as enter, but with backspace
     this.pressBack = () => {
         this.display.addEventListener('keypress', (e) => {
             if(e.keyCode === 8) this.del();
         });
     };
 
+    // writes on display and take the focus back to display every 'equals pressed'
     this.displayBtn = el => {
         this.display.value += el.innerText;
         this.display.focus();
     };
 
+    //clear and del
     this.clear = () => this.display.value = '';
     this.del = () => this.display.value = this.display.value.slice(0, -1);
 
@@ -57,5 +64,5 @@ function Calculadora() {
     };
 };
 
-const calculadora = new Calculadora();
-calculadora.start();
+const calculator = new Calculator();
+calculator.start();
